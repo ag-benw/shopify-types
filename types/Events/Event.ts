1a5c66f4214b724e5ee2cc6060e08917ea2aa27b
Benjamin Wilson <benjamin.wilson@acidgreen.com.au>
2023-04-17 11:09:55 +1000

    created events scope

---
+++ types/Events/Event.ts
@@ -0,0 +1,12 @@
+export interface EventsResource {
+  arguments: string
+  body: string | null
+  created_at: string
+  id: number
+  description: string
+  path: string
+  message: string
+  subject_id: number
+  subject_type: string
+  verb: string
+}
---
+++ types/Events/Webhook.ts
@@ -0,0 +1,12 @@
+export interface WebhookResource {
+  address: string
+  api_version: string
+  created_at: string
+  fields: string[]
+  format: string
+  id: number
+  metafield_namespaces: string[]
+  private_metafield_namespaces: string[]
+  topic: string
+  updated_at: string
+}
